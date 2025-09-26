5359271bf5a753b2ed34b137e5818a00c6d4ea1b
Pedro Henrique Fonseca <pluto@MacBook-Pro-de-Pedro.local>
2025-09-25 21:12:44 -0300

    Reorganização de pastas e arquivos

--- Arquivo de Estudo.py
+++
@@ -1,6 +0,0 @@
-#Captando as informações
-preço = float(input('Qual o valor atual do produto que deseja comprar? '))
-desconto = float(input('Qual o desconto, em porcentagem, a ser aplicado na compra? '))
-
-#Apresentando as informações calculadas
-print (f'O preço do produto com {desconto}% de desconto será de R${round(preço - (preço * (desconto/100)),2)}.')                        
